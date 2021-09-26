10ccab71afecc0acf0bacf3cfd3ee1f19c0b6c05
DrBanana419 <73282866+DrBanana419@users.noreply.github.com>
2021-09-26 15:20:09 +0000

    Create 1966disc.py

---
+++ cobra/1966disc.py
@@ -0,0 +1,62 @@
+print("Secretariat of president:")
+w1=int(input(": "))
+if w1==19:
+    print("Nice!")
+else:
+    print("wrong")
+
+print("Defense")
+w2= int(input(""))
+if w2==81:
+    print("Nice!")
+else:
+    print("Wrong")
+
+print("Industry")
+w3= int(input(""))
+if w3==257:
+    print("Nice!")
+else:
+    print("Wrong")
+
+print("Home Affairs")
+w4= int(input(""))
+if w4==227:
+    print("Nice!")
+else:
+    print("Wrong")
+
+print("Information")
+w5= int(input(""))
+if w5==201:
+    print("Nice!")
+else:
+    print("Wrong")
+
+print("Education")
+w6= int(input(""))
+if w6==273:
+    print("Nice!")
+else:
+    print("Wrong")
+
+print("Health")
+w7= int(input(""))
+if w7==19:
+    print("Nice!")
+else:
+    print("Wrong")
+
+print("Law")
+w8= int(input(""))
+if w8==35:
+    print("Nice!")
+else:
+    print("Wrong")
+
+print("Agriculture")
+w9= int(input(""))
+if w9==21:
+    print("Nice!")
+else:
+    print("Wrong")
