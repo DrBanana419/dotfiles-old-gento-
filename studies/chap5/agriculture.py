02a4d7735226637c4c20ffe3bcdb905148e5eb96
DrBanana419 <73282866+DrBanana419@users.noreply.github.com>
2021-09-26 15:24:00 +0000

    Create agriculture.py

---
+++ studies/chap5/agriculture.py
@@ -0,0 +1,27 @@
+print("what is produced in West-Bengal, Bangladesh and Myanmar?")
+w1=input("")
+if w1=="paddy, wheat":
+    print("Nice!")
+else:
+    print("Wrong")
+
+print("What is produced in abundance in Bangladesh?")
+w1=input("")
+if w1=="paddy, potatoes, jute":
+    print("Nice!")
+else:
+    print("Wrong")
+
+print("What other crops are grown?")
+w1=input("")
+if w1=="wheatt, corn, mustard":
+    print("Nice!")
+else:
+    print("Wrong")
+
+print("What is grown in India and Myanmar?")
+w1=input("")
+if w1=="cotton, tea, lentil, chili":
+    print("Nice!")
+else:
+    print("Wrong")
